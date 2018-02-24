2be83fedb8179e32026f5725d8c851b8b0bc0d2e
radguru <sgsingh98@yahoo.com>
2018-02-24 16:44:49 +0100

    Dependencies done

--- src/ApssInfoBot.java
+++ src/ApssInfoBot.java
@@ -1,5 +1,3 @@
-
-
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-
-
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
